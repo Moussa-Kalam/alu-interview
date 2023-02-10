76fa623090a45f7d9b5b0223872d98f4045e3d22
Moussa-Kalam <m.amzat@alustudent.com>
2023-02-10 15:41:48 +0200

    pep8 validation

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -12,14 +12,10 @@ def minOperations(n):
 
     if not n or n <= 1:
         return 0
-    
     operations = 0
     for time in range(2, n+1):
         while(n % time == 0):
             operations += time
             n = n / time
     return operations
-
-
-# print(minOperations(12))
-# print(minOperations(4))
+    
