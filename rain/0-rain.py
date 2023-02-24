432fc110f4fd37d413d6f0cd14f3c668961e2154
Moussa-Kalam <m.amzat@alustudent.com>
2023-02-24 19:09:40 +0200

    pep8 compliant

--- rain/0-rain.py
+++ rain/0-rain.py
@@ -3,6 +3,7 @@
 Rain
 """
 
+
 def rain(walls):
     """
     Given a list of non-negative integers
@@ -12,19 +13,23 @@ def rain(walls):
     n = len(walls)
     if n == 0:
         return 0
-    # Initialize two arrays to store maximum height on left and right sides of each wall
+    # Initialize two arrays to store maximum height on left and right sides of
+    # each wall
     left_max = [0] * n
     right_max = [0] * n
-    # Calculate maximum height on left side for each wall and store it in left_max
+    # Calculate maximum height on left side for each wall and store it in
+    # left_max
     left_max[0] = walls[0]
     for i in range(1, n):
         left_max[i] = max(left_max[i - 1], walls[i])
-    # Calculate maximum height on right side for each wall and store it in right_max
+    # Calculate maximum height on right side for each wall and store it in
+    # right_max
     right_max[n - 1] = walls[n - 1]
     for i in range(n - 2, -1, -1):
         right_max[i] = max(right_max[i + 1], walls[i])
     water = 0
-    # Iterate through array one more time to calculate the amount of water trapped between each wall
+    # Iterate through array one more time to calculate the amount of water
+    # trapped between each wall
     for i in range(n):
         water += min(left_max[i], right_max[i]) - walls[i]
     return water
